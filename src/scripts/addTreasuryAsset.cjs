ab75b25706da19ebbb8858bc80950825ac8f6299
Andrew Wilkinson <hello@andrewwilkinson.me>
2024-09-16 08:32:53 +0100

    Update addTreasuryAsset.cjs

--- src/scripts/addTreasuryAsset.cjs
+++ src/scripts/addTreasuryAsset.cjs
@@ -9,32 +9,11 @@ const db = admin.firestore();
 
 const assets = [
   {
-    symbol: "DYDX",
-    quantity: 338051.0,
-    href: "https://dydx.trade/",
-    imgSrc: "https://assets.coingecko.com/coins/images/32594/standard/dydx.png?1698673495",
-    id: "dydx-chain",
-  },
-  {
-    symbol: "CVXCRV",
-    quantity: 1183675.0,
-    href: "https://www.convexfinance.com/",
-    imgSrc: "https://assets.coingecko.com/coins/images/15586/standard/convex-crv.png?1696515222",
-    id: "convex-crv",
-  },
-  {
-    symbol: "AURA",
-    quantity: 150000.0,
-    href: "https://aura.finance/",
-    imgSrc: "https://assets.coingecko.com/coins/images/25942/standard/logo.png?1696525021",
-    id: "aura-finance",
-  },
-  {
-    symbol: "CVX",
-    quantity: 17763.35,
-    href: "https://www.convexfinance.com/",
-    imgSrc: "https://assets.coingecko.com/coins/images/15585/standard/convex.png?1696515221",
-    id: "convex-finance",
+    symbol: "",
+    quantity: 0,
+    href: "", 
+    imgSrc: "", // Token logo url
+    id: "", // CoinGecko API ID
   },
 ];
 
